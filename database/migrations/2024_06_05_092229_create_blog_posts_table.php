0fd48d1fa7b39abc5542b5cf700019e09a5be30b
mesan237 <negokamgaing@gmail.com>
2024-06-06 07:57:26 +0100

    mgrations fixed

--- database/migrations/2024_06_05_092229_create_blog_posts_table.php
+++ database/migrations/2024_06_05_092229_create_blog_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('content');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('category_id')->constrained('categories')->onDelete('set null');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->string('tags')->nullable();
             $table->string('status');
             $table->date('published_date')->nullable();
