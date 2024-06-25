498ba40a760c0a3070a827714de540329fa82725
mesan237 <negokamgaing@gmail.com>
2024-06-25 06:17:20 +0100

    add image insert and test editor on the frontend of product page

--- resources/js/Pages/admin/Product/AddProduct.jsx
+++ resources/js/Pages/admin/Product/AddProduct.jsx
@@ -1,6 +1,9 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 import { useForm, usePage } from "@inertiajs/react";
+
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import {
   Breadcrumbs,
   Button,
@@ -16,7 +19,7 @@ const AddProduct = function ({ auth }) {
   // updating informations
   const { data, setData, post, processing, errors } = useForm({
     name: "",
-    description: "",
+    longDescription: "",
     shortDescription: "",
     category: "",
     subCategory: "",
@@ -41,6 +44,24 @@ const AddProduct = function ({ auth }) {
     } catch (error) {
       console.error("There was an error", error);
     }
+  };
+
+  const [images, setImages] = useState([]);
+
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    const newImages = files.map((file) => {
+      return {
+        file,
+        url: URL.createObjectURL(file),
+      };
+    });
+    setImages([...images, ...newImages]);
+  };
+
+  const handleDelete = (index) => {
+    const newImages = images.filter((image, i) => i !== index);
+    setImages(newImages);
   };
   return (
     <AuthenticatedLayout user={auth.user}>
@@ -102,7 +123,7 @@ const AddProduct = function ({ auth }) {
                 </div>
                 <div className="col-span-6 sm:col-span-3 flex flex-col gap-3">
                   <Typography variant="h6" className="-mb-1 dark:text-white">
-                    Stock
+                    Quantity
                   </Typography>
                   <Input
                     size="lg"
@@ -117,18 +138,19 @@ const AddProduct = function ({ auth }) {
                 </div>
                 <div className="col-span-6 sm:col-span-3 flex flex-col gap-3">
                   <Typography variant="h6" className="-mb-1 dark:text-white">
-                    Description
+                    short description
                   </Typography>
                   <Input
                     size="lg"
-                    name="description"
-                    placeholder="description"
-                    value={data.description}
+                    name="short-description"
+                    value={data.shortDescription}
                     className=" !border-t-blue-gray-200 focus:!border-t-gray-900 input-default"
                     labelProps={{
                       className: "before:content-none after:content-none",
                     }}
-                    onChange={(e) => setData("description", e.target.value)}
+                    onChange={(e) =>
+                      setData("shortDescription", e.target.value)
+                    }
                   />
                 </div>
                 <div className="col-span-3 sm:col-span-6 flex gap-3">
@@ -312,6 +334,63 @@ const AddProduct = function ({ auth }) {
                     }}
                     onChange={(e) => setData("warranty", e.target.value)}
                   />
+                </div>
+                <div className="col-span-3 sm:col-span-6 gap-3">
+                  <div>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="mb-2 font-medium"
+                    >
+                      Long Description:
+                    </Typography>
+                    <ReactQuill
+                      value={data.longDescription}
+                      className="dark:bg-components-dark py-1"
+                      onChange={(e) =>
+                        setData("longDescription", e.target.value)
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="col-span-3 sm:col-span-6 gap-3">
+                  <div>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="mb-2 font-medium"
+                    >
+                      Image insertion:
+                    </Typography>
+                    <input
+                      type="file"
+                      multiple
+                      onChange={handleFileChange}
+                      className="block w-full text-sm text-gray-500
+                   file:mr-4 file:py-2 file:px-4
+                   file:rounded-full file:border-0
+                   file:text-sm file:font-semibold
+                   file:bg-violet-50 file:text-violet-700
+                   hover:file:bg-violet-100"
+                    />
+                    <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                      {images.map((image, index) => (
+                        <div key={index} className="relative">
+                          <img
+                            src={image.url}
+                            alt={`Uploaded ${index}`}
+                            className="w-full h-auto object-cover rounded-lg"
+                          />
+                          <button
+                            onClick={() => handleDelete(index)}
+                            className="absolute top-2 right-2 bg-red-500 text-white rounded-full p-1 focus:outline-none focus:ring-2 focus:ring-red-300"
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
                 </div>
 
                 <div className="col-span-6 sm:col-full">
